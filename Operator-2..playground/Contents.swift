01f45a09850ebf539b9b7486a1d1ca10cd228960
JustinSheng <justin.wang4@icloud.com>
2024-04-06 15:52:14 +0800

    More Transforming Operators

--- Operator-2..playground/Contents.swift
+++ Operator-2..playground/Contents.swift
@@ -3,7 +3,42 @@ import Combine
 
 var subscriptions = Set<AnyCancellable>()
 
-<#Add your code here#>
+example(of: "scan") {
+	var dailyGainLoss: Int { .random(in: -10...10) }
+	
+	let august2019 = (0..<22)
+		.map{ _ in dailyGainLoss }
+		.publisher
+	
+	august2019
+		.scan(50) { latest, current in
+			max(0, latest + current)
+		}
+		.sink(receiveValue: { _ in })
+		.store(in: &subscriptions)
+	
+}
+
+example(of: "flatMap") {
+	
+	let charlotte = Chatter(name: "Charlotte", message: "Hi, I'm Charlotte!")
+	let james = Chatter(name: "James", message: "Hi, I'm James!")
+	
+	let chat = CurrentValueSubject<Chatter, Never>(charlotte)
+	
+	chat
+		.flatMap { $0.message }
+		.sink(receiveValue: { print($0) })
+		.store(in: &subscriptions)
+	
+	charlotte.message.value = "Charlotte: How's it going?"
+	chat.value = james
+	
+	james.message.value = "James: Doing great. You?"
+	charlotte.message.value = "Charlotte: I'm doing fine, thanks."
+	
+}
+
 
 /// Copyright (c) 2019 Razeware LLC
 ///
