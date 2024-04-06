07101919d1d5bbf54d3e3c4fd44fb52f093d7bec
JustinSheng <justin.wang4@icloud.com>
2024-04-06 14:38:34 +0800

    Subscriber Operators and Subjects

--- Begin.playground/Contents.swift
+++ Begin.playground/Contents.swift
@@ -21,6 +21,80 @@ example(of: "NotificationCenter") {
 	
 }
 
+example(of: "Just") {
+	
+	let just = Just("Hello World")
+	
+	just
+		.sink (receiveCompletion: {
+			print("Received Completion", $0)
+		}, receiveValue: {
+			print("Received Value", $0)
+		})
+		.store(in: &subscriptions)
+	
+}
+
+example(of: "assign(to:on:)") {
+	class SomeObject {
+		var value: String = "" {
+			didSet {
+				print(value)
+			}
+		}
+	}
+	
+	let object = SomeObject()
+	
+	["Hello", "World"].publisher
+		.assign(to: \.value, on: object)
+		.store(in: &subscriptions)
+}
+
+example(of: "PassthroughSubject") {
+	let subject = PassthroughSubject<String, Never>()
+	
+	subject.sink(receiveValue: { print($0) })
+		.store(in: &subscriptions)
+	
+	subject.send("Hello")
+	subject.send("World!")
+	subject.send(completion: .finished)
+	subject.send("Still there?")
+}
+
+example(of: "CurrentValueSubject") {
+	let subject = CurrentValueSubject<Int, Never>(0)
+	
+	subject
+		.print()
+		.sink(receiveValue: { print($0) })
+		.store(in: &subscriptions)
+	
+	print(subject.value)
+	
+	subject.send(1)
+	subject.send(2)
+	
+	print(subject.value)
+	
+	subject.send(completion: .finished)
+}
+
+example(of: "Type erasure") {
+	let subject = PassthroughSubject<Int, Never>()
+	
+	let publisher = subject.eraseToAnyPublisher()
+	
+//	publisher.send(0)
+	
+	publisher.sink(receiveValue: { print($0) })
+		.store(in: &subscriptions)
+	
+	subject.send(0)
+	
+}
+
 /// Copyright (c) 2020 Razeware LLC
 ///
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
